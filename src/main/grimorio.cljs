7f83ec16a30fadace888a056d7f587ab3dedd352
jelohe <jsus.90@gmail.com>
2025-06-09 17:43:13 +0200

    Add basic routing capabilities

--- src/main/grimorio.cljs
+++ src/main/grimorio.cljs
@@ -1,9 +1,18 @@
 (ns grimorio
   (:require [state]
             [components.header :refer [header]]
+            [reagent.core :as reagent]
             [components.sidebar :refer [sidebar]]
             [components.content :refer [content]]
+            [reitit.core :as r]
+            [reitit.frontend :as rf]
+            [reitit.frontend.easy :as rfe]
+            [reitit.coercion.spec :as rss]
             [reagent.dom :as rdom]))
+
+(defn login []
+  [:div
+   [:p "Work in progress :)"]])
 
 (defn grimorio []
   (let [notes @state/opened-notes]
@@ -13,9 +22,33 @@
       (sidebar notes)
       (content (state/selected-note notes))]]))
 
+(defonce current-page (reagent/atom nil))
+
+(def routes
+  [["/"         {:name ::login
+                 :view login}]
+   ["/grimorio" {:name ::grimorio
+                 :view grimorio}]])
+
+(defn init-routes! []
+  (rfe/start!
+    (rf/router routes)
+    (fn [match]
+      (reset! current-page match))
+    {:use-fragment false}))
+
+(defn app []
+  (let [page @current-page]
+    (if page
+      [(:view (:data page))]
+      [:div "Loading..."])))
+
 (defn render! []
-  (rdom/render [grimorio] (.getElementById js/document "root")))
+  (rdom/render [app] (js/document.getElementById "root")))
 
-(defn init! [] (render!))
+(defn init! []
+  (init-routes!)
+  (render!))
 
-(defn ^:dev/after-load reload! [] (render!))
+(defn ^:dev/after-load reload! []
+  (render!))
