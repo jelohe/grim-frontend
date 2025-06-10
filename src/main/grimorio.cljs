7e6f6be44abf182d20d531424adcdd474dff948a
jelohe <jsus.90@gmail.com>
2025-06-10 06:00:37 +0200

    Create login page markup

--- src/main/grimorio.cljs
+++ src/main/grimorio.cljs
@@ -1,53 +1,18 @@
 (ns grimorio
   (:require [state]
-            [components.header :refer [header]]
+            [router]
+            [reagent.dom :as rdom]
             [reagent.core :as reagent]
+            [components.header :refer [header]]
             [components.sidebar :refer [sidebar]]
             [components.content :refer [content]]
-            [reitit.core :as r]
-            [reitit.frontend :as rf]
-            [reitit.frontend.easy :as rfe]
-            [reitit.coercion.spec :as rss]
-            [reagent.dom :as rdom]))
-
-(defn login []
-  [:div
-   [:p "Work in progress :)"]])
-
-(defn grimorio []
-  (let [notes @state/opened-notes]
-    [:<>
-     (header)
-     [:main
-      (sidebar notes)
-      (content (state/selected-note notes))]]))
-
-(defonce current-page (reagent/atom nil))
-
-(def routes
-  [["/"         {:name ::login
-                 :view login}]
-   ["/grimorio" {:name ::grimorio
-                 :view grimorio}]])
-
-(defn init-routes! []
-  (rfe/start!
-    (rf/router routes)
-    (fn [match]
-      (reset! current-page match))
-    {:use-fragment false}))
-
-(defn app []
-  (let [page @current-page]
-    (if page
-      [(:view (:data page))]
-      [:div "Loading..."])))
+            ))
 
 (defn render! []
-  (rdom/render [app] (js/document.getElementById "root")))
+  (rdom/render [router/app] (js/document.getElementById "root")))
 
 (defn init! []
-  (init-routes!)
+  (router/init!)
   (render!))
 
 (defn ^:dev/after-load reload! []
---
+++ src/main/router.cljs
@@ -0,0 +1,55 @@
+(ns router
+  (:require [components.header :refer [header]]
+            [components.sidebar :refer [sidebar]]
+            [components.content :refer [content]]
+            [reagent.core :as reagent]
+            [reagent.dom :as rdom]
+            [reitit.frontend :as rf]
+            [reitit.frontend.easy :as rfe]
+            ))
+
+(defonce current-page (reagent/atom nil))
+
+(defn handle-login []
+  (rfe/push-state ::grimorio))
+
+(defn login []
+  [:div
+   [:form
+     [:h1 "Grimorio"]
+     [:input {:id "email" 
+              :placeholder "Email"
+              :type "text"}]
+     [:input {:id "password" 
+              :placeholder "Password"
+              :type "password"}]
+     [:input {:value "Open"
+              :type "submit"
+              :on-click #(handle-login)}]]])
+
+(defn grimorio []
+  (let [notes @state/opened-notes]
+    [:<>
+     (header)
+     [:main
+      (sidebar notes)
+      (content (state/selected-note notes))]]))
+
+(def routes
+  [["/"         {:name ::login
+                 :view login}]
+   ["/grimorio" {:name ::grimorio
+                 :view grimorio}]])
+
+(defn init! []
+  (rfe/start!
+    (rf/router routes)
+    (fn [match]
+      (reset! current-page match))
+    {:use-fragment false}))
+
+(defn app []
+  (let [page @current-page]
+    (if page
+      [(:view (:data page))]
+      [:div "Loading..."])))
